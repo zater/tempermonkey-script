a8d710111b3a4f75f29a25f4ac1d1264799b60a3
zater <zater@vip.qq.com>
2017-06-27 13:44:32 +0800

    Update exloginer.user.js

--- exloginer.user.js
+++ exloginer.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://exhentai.org/
+// @match        https://ex/
 //@require  https://code.jquery.com/jquery-3.2.1.min.js
 // @grant        none
 // ==/UserScript==
