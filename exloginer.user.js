081ec7ec733fce5a25a25d6f9860079d45d69d3c
zater <zater@vip.qq.com>
2017-06-27 11:15:35 +0800

    Update exloginer.user.js

--- exloginer.user.js
+++ exloginer.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author      zater
 // @match        https://ehantai/
 //@require  https://code.jquery.com/jquery-3.2.1.min.js
 // @grant        none
